db53bd276d2eb9272c006d9d2782ee5812658ebc
Parvez13 <prvzsohail@gmail.com>
2023-09-04 10:05:28 +0530

    update files

--- src/configuration/config.py
+++ src/configuration/config.py
@@ -11,3 +11,8 @@ class EnvironmentVariable():
 
 env_var = EnvironmentVariable()
 mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
+
+# MLFLOw Experimentation
+mlflow_tracking_ui: https: // dagshub.com/prvzsohail/End-to-End-Employee-Job-Satisfaction-Project.mlflow
+mlflow_USERNAME: prvzsohail
+mlflow_PASSWORD: 693d1810cba447c3876af51f98d347c4c14a7972
